8cae2e5e8c65f440085bb91c31366f6a9c91b4ac
pcbje <petter.bjelland@gmail.com>
2013-11-15 21:32:32 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='Petter Bjelland',
     version='0.1',
     author_email='petter.bjellans@hig.no',
-    description='API for generating Maltego MTGX file.',
+    description='API for generating Maltego mtgx file.',
     license='Apache2',    
     scripts=[],
     zip_safe=False,
