8dd9e60b13d0f5d578f1feadb8d43cce49f405e9
Petter Christian Bjelland <petter.bjelland@gmail.com>
2013-11-15 20:15:34 +0100

    setup file

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='pymtgx',
+    author='Petter Bjelland',
+    version='0.1',
+    author_email='petter.bjellans@hig.no',
+    description='API for generating Maltego MTGX file.',
+    license='Apache2',    
+    scripts=[],
+    zip_safe=False,
+    package_data={},
+    install_requires=[
+        'networkx'
+    ]
+)
